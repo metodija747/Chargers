045085228bb731bf12ce6ed4643f9af68e9036a3
Metodija747 <metodijabucevski@yahoo.com>
2022-01-05 19:34:14 +0100

    Dobro

--- api/src/main/java/si/fri/rso/charging/api/ElectricStationApplication.java
+++ api/src/main/java/si/fri/rso/charging/api/ElectricStationApplication.java
@@ -1,8 +1,15 @@
 package si.fri.rso.charging.api;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@OpenAPIDefinition(info = @Info(title = "Charger details API1", version = "v1", contact = @Contact(email = "mb8696@student.uni-lj.si"), license = @License(name="dev"), description = "API for managing chargers."), servers = @Server(url ="http://20.127.141.29/charge"))
 @ApplicationPath("/v1")
 public class ElectricStationApplication extends Application {
 }
--- api/src/main/java/si/fri/rso/charging/api/resources/ElStationResource.java
+++ api/src/main/java/si/fri/rso/charging/api/resources/ElStationResource.java
@@ -5,9 +5,18 @@ import com.kumuluz.ee.cors.annotations.CrossOrigin;
 import com.kumuluz.ee.logs.cdi.Log;
 import org.eclipse.microprofile.metrics.annotation.Metered;
 import org.eclipse.microprofile.metrics.annotation.Timed;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import si.fri.rso.charging.models.Chargers;
 import si.fri.rso.charging.services.producers.ChargerBean;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -18,7 +27,7 @@ import javax.ws.rs.core.UriInfo;
 import java.util.List;
 
 @Log
-@RequestScoped
+@ApplicationScoped
 @Path("/chargers")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -31,6 +40,12 @@ public class ElStationResource {
     @Context
     protected UriInfo uriInfo;
 
+    @Operation(description = "Get for all chargers information.", summary = "Get all chargers")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "List of chargers and their details",
+                    content = @Content(schema = @Schema(implementation = Chargers.class, type = SchemaType.ARRAY))
+            )})
     @GET
     @Metered
     public Response getChargers() {
@@ -40,11 +55,21 @@ public class ElStationResource {
         return Response.ok(chargers).build();
     }
 
+    @Operation(description = "Get charger information.", summary = "Get details for specific charger")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "Specific charger details",
+                    content = @Content(
+                            schema = @Schema(implementation = Chargers.class))
+            ),
+            @APIResponse(responseCode = "404", description = "Resource not found .")
+    })
     @GET
     @Path("/{chargerId}")
     @Log
     @Timed
-    public Response getCharger(@PathParam("chargerId") String chargerId) {
+    public Response getCharger(@Parameter(description = "Charger ID.", required = true)
+            @PathParam("chargerId") String chargerId) {
 
         Chargers charger = chargerBean.getCharger(chargerId);
 
@@ -54,10 +79,21 @@ public class ElStationResource {
 
         return Response.status(Response.Status.OK).entity(charger).build();
     }
+
+    @Operation(description = "Add new charger.", summary = "Add charger")
+    @APIResponses({
+            @APIResponse(responseCode = "201",
+                    description = "Charger successfully added."
+            ),
+            @APIResponse(responseCode = "400", description = "Bas request .")
+    })
     @POST
     @Log
     @Timed
-    public Response createCharger(Chargers charger) {
+    public Response createCharger(@RequestBody(
+            description = "DTO object with charger details.",
+            required = true, content = @Content(
+            schema = @Schema(implementation = Chargers.class))) Chargers charger) {
 
         if ((charger.getName() == null || charger.getName().isEmpty()) || (charger.getLocation() == null
                 || charger.getLocation().isEmpty())) {
@@ -73,9 +109,20 @@ public class ElStationResource {
         }
     }
 
+
+    @Operation(description = "Update charger details.", summary = "Update charger's info")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Details successfully updated."
+            ),
+            @APIResponse(responseCode = "400", description = "Bas request."),
+            @APIResponse(responseCode = "304", description = "Resource not modified.")
+    })
     @PUT
     @Path("/{chargerId}")
-    public Response putComment(@PathParam("chargerId") String chargerId, Chargers charger) {
+    public Response putComment(@Parameter(description = "Charger ID.", required = true)
+            @PathParam("chargerId") String chargerId, Chargers charger) {
 
         charger = chargerBean.putCharger(chargerId, charger);
 
@@ -88,10 +135,23 @@ public class ElStationResource {
                 return Response.status(Response.Status.NOT_MODIFIED).build();
         }
     }
+
+    @Operation(description = "Delete charger.", summary = "Delete charger")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "410",
+                    description = "Charger successfully deleted."
+            ),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Not found."
+            )
+    })
     @DELETE
     @Path("{chargerId}")
     @Log
-    public Response deleteCustomer(@PathParam("chargerId") String chargerId) {
+    public Response deleteCustomer(@Parameter(description = "Charger ID.", required = true)
+            @PathParam("chargerId") String chargerId) {
 
         boolean deleted = chargerBean.deleteCharger(chargerId);
 

419fd6ddf9875d53c09a870b4ae2c2d1bed8cb83
Metodija747 <metodijabucevski@yahoo.com>
2022-01-06 01:17:30 +0100

    Dobro

--- api/src/main/java/si/fri/rso/charging/api/ElectricStationApplication.java
+++ api/src/main/java/si/fri/rso/charging/api/ElectricStationApplication.java
@@ -1,5 +1,6 @@
 package si.fri.rso.charging.api;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
@@ -10,6 +11,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @OpenAPIDefinition(info = @Info(title = "Charger details API1", version = "v1", contact = @Contact(email = "mb8696@student.uni-lj.si"), license = @License(name="dev"), description = "API for managing chargers."), servers = @Server(url ="http://20.127.141.29/charge"))
+@RegisterService
 @ApplicationPath("/v1")
 public class ElectricStationApplication extends Application {
 }
